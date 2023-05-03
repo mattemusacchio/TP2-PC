f848d2293c18d7520332d72a9c6021348a1089de
Carl <106560791+Carla1nf@users.noreply.github.com>
2023-05-03 17:11:09 -0300

    Add files via upload

---
+++ TestNew.py
@@ -0,0 +1,26 @@
+
+def Cifrado(msg, clave):
+  valuesPerLetter = list(map(chr, range(97, 123))) * 2
+  encryptedValue = []
+  indexEncrypted = 0
+  newClave = clave.casefold()
+  newMensaje = msg.casefold()
+  for letter in newMensaje:
+     if(letter.isalpha()):
+       encryptedValue.append(valuesPerLetter[(ord(letter) - ord("a")) + (ord(newClave[indexEncrypted]) - ord("a"))])
+       if(len(newClave) - 1 == indexEncrypted):
+        indexEncrypted = 0
+       else: 
+        indexEncrypted += 1
+     else: 
+      encryptedValue.append(" ")
+
+  result = "".join(encryptedValue)
+  print(result)
+
+      
+
+
+
+
+Cifrado("Este es un mensaje super secreto.", "clavesecreta")
